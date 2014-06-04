d1e6439cbca701811e5667ac6769557351f6ad37
Azusa <tonnyazusa@gmail.com>
2014-06-05 02:23:40 +0900

    Lesson 3 Complete

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var http = require("http");
+var argv = require("minimist")(process.argv.splice(2));
+var port = argv.port || 4000;
 
 function myexpress(){
-	return function(req, res, next){
+	//使用app.handle来对app进行回调使用
+	function app(req, res, next){
+		app.handle(req, res, next);
+	}
+	//处理请求和应答
+	app.handle = function(req, res, next){
 		if (req.url == "/foo"){
 			res.statusCode = 404;
 			res.end();
 		} else {
 			next();
 		}
-	}
+	};
+	//处理端口
+	app.listen = function(port, done){
+		var server = http.createServer(app);
+		server.listen(port, done);
+		return server;
+	};
+
+	return app;
 }
 
 module.exports = myexpress;
--- test/app_spec.js
+++ test/app_spec.js
@@ -2,16 +2,32 @@ var express = require("../");
 var request = require("supertest");
 var http = require("http");
 
-describe("Implement Empty App",function() {
+describe("Implement Empty App", function() {
 	var app = express();
-	describe("as handler to http.createServer",function() {
+	describe("as handler to http.createServer", function() {
 		// write test case here
-		it("responds to /foo with 404",function(done){
+		it("responds to /foo with 404", function(done){
 			var server = http.createServer(app);
 			request(server)
 				.get("/foo")
 				.expect(404)
 				.end(done);
+		});
+		it("create the app.listen method", function(){
+			var port = 8000;
+			var server;
+
+			before(function(done){
+				server = app.listen(port, done);
+			});
+
+			it("should return an http server", function(done){
+				expect(server).to.be.instanceof(http.server);
+			});
+
+			it("respond to /foo with 404", function(done){
+				request("http://localhost:" + port).get("/foo").expect(404).end(done);	
+			});
 		});
 	});
 });
